bda62d9393efd7208d97ceb02b9b525ee797758f
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2022-10-26 22:37:19 +0300

    26.10.22

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+console.log(SimpleLightbox);
+const gallery = document.querySelector('.gallery');
+const imgGallery = CreateAndRenderMarkupGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imgGallery);
+
+function CreateAndRenderMarkupGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div >
+`;
+    })
+    .join('');
+}
+
+// gallery.addEventListener('click', onImageClick);
+
+// function onImageClick(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(`
+//     <img src = "${event.target.dataset.source}"> width = "800" height = "600"
+//     `);
+//   instance.show();
+//   console.log(event);
+// }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+console.log(localStorage);
 
+localStorage.setItem('key', JSON.stringify({ name: 'mango', egt: 2 }));
+
+const savedData = localStorage.getItem('key');
+console.log('savedData', savedData);
+
+const parsedData = JSON.parse(savedData);
+console.log('parsedData', parsedData);
