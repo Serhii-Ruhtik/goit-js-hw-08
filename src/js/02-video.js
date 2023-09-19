4e08a45a3f793e450eb09009db7f30ff685af069
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-09-19 23:04:53 +0300

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import vimeoPlayer from '@vimeo/player';
 
+const iframeVideo = document.querySelector('#vimeo-player');
+
+const player = new vimeoPlayer(iframeVideo);
+const throttler = require('lodash.throttle');
+
+player.on('timeupdate', throttler(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', Math.round(data.seconds));
+}
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,3 @@
-// console.log(localStorage);
-
-// localStorage.setItem('key', JSON.stringify({ name: 'mango', egt: 2 }));
-
-// const savedData = localStorage.getItem('key');
-// console.log('savedData', savedData);
-
-// const parsedData = JSON.parse(savedData);
-// console.log('parsedData', parsedData);
-
-// ===========================================
 const STORAGE_KEY = 'feedback-form-state';
 
 
@@ -48,51 +37,3 @@ function populateTextarea() {
     refs.textarea.value = savedMessage;
   }
 }
-
-// const STORAGE_KEY = 'feedback-msg';
-
-// const refs = {
-//   form: document.querySelector('.js-feedback-form'),
-//   textarea: document.querySelector('.js-feedback-form  textarea'),
-// };
-
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
-
-// populateTextarea();
-
-// /*
-//  * - Останавливаем поведение по умолчанию
-//  * - Убираем сообщение из хранилища
-//  * - Очищаем форму
-//  */
-// function onFormSubmit(evt) {
-//   evt.preventDefault();
-
-//   console.log('Отправляем форму');
-//   evt.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// /*
-//  * - Получаем значение поля
-//  * - Сохраняем его в хранилище
-//  * - Можно добавить throttle
-//  */
-// function onTextareaInput(evt) {
-//   const message = evt.target.value;
-
-//   localStorage.setItem(STORAGE_KEY, message);
-// }
-
-// /*
-//  * - Получаем значение из хранилища
-//  * - Если там что-то было, обновляем DOM
-//  */
-// function populateTextarea() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-//   if (savedMessage) {
-//     refs.textarea.value = savedMessage;
-//   }
-// }
